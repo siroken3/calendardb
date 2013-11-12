70b8e628666662e494ef6f7cdad9e92b177b603c
Kenichi Sasaki <sasaki-k@klab.com>
2013-11-13 01:05:15 +0900

    分割されるパターンのテストケース追加

--- test/btree_span_test.py
+++ test/btree_span_test.py
@@ -1,6 +1,7 @@
 import index
 from index import BTree, BPlusTree
 
+
 def test_additions():
     bt = BTree(20)
     rl = [index.Span(a, a + 5) for a in range(0, 100, 5)]
@@ -8,8 +9,9 @@ def test_additions():
         bt.insert(r)
         assert list(bt) == rl[:i + 1]
 
+
 def test_additions_sorted():
-    bt= BPlusTree(20)
+    bt = BPlusTree(20)
     rl = [index.Span(a, a + 5) for a in range(0, 100, 5)]
 
     for item in rl:
@@ -20,3 +22,27 @@ def test_additions_sorted():
 
     assert rl == list(bt)
 
+
+def test_contains():
+    bt = BPlusTree(20)
+    key_a = index.Span(1, 6)
+    key_b = index.Span(6, 10)
+    bt.insert(key_a, "a_value")
+    bt.insert(key_b, "b_value")
+    assert key_a in bt
+    assert key_b in bt
+    assert bt[key_a] == "a_value"
+    assert bt[key_b] == "b_value"
+
+def test_split():
+    bt = BPlusTree(20)
+    key_a = index.Span(1, 6)
+    key_b = index.Span(3, 10)
+    key_a_prime = index.Span(1, 3)
+    bt.insert(key_a, "a_value")
+    bt.insert(key_b, "b_value")
+    assert not (key_a in bt)
+    assert key_a_prime in bt
+    assert key_b in bt
+    assert bt[key_a_prime] == "a_value"
+    assert bt[key_b] == "b_value"
